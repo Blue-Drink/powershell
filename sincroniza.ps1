a71a741ab75b19114884ddf781891218daba987d
Blue-Drink <justme1random@gmail.com>
2025-09-29 10:12:55 +0200

    Última versión

--- sincroniza.ps1
+++ sincroniza.ps1
@@ -5,4 +5,6 @@ git add .
 $a = read-host -prompt "Introduce mensaje para commit"
 while( ($Null -eq $a ) -or ( $a -eq '' ) ){ $a = read-host -prompt "Introduce mensaje para commit" }
 
-git commit -m "$a"
+git commit -m $a
+
+git push --force origin main
