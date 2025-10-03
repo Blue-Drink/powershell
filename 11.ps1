d115314c61e51b6f49ca6214699b1ff109d174d0
Blue-Drink <justme1random@gmail.com>
2025-10-03 09:59:15 +0200

    Como 9 pero con estilos. Aviso añadido a README

---
+++ 11.ps1
@@ -0,0 +1,20 @@
+$p = Get-Process
+$l = $p.length
+$css = get-content -path "./estilo.css"
+$cadena = "<html><head><style>" + $css + "</style></head><body><center><table>"
+$ruta = "C:\xampp\htdocs\power\"
+
+for( $i = 0 ; $i -lt $l ; $i++ ){
+    $cadena += "<tr><td>" + $p[ $i ].ProcessName + "</td><td>" + $p[ $i ].id + "</td></tr>";
+}
+$cadena += "</table></center></body></html>"
+
+if( -not( Test-Path $ruta -PathType Container ) ){
+    try{ New-Item -Path $ruta -ItemType Directory}
+    catch{
+        Write-Host "No se ha podido crear el directorio." -ForegroundColor Red;
+        Write-Host $_.Exception.Message -ForegroundColor Red;
+    }
+}
+
+Set-Content -Path $ruta\index.html -Value $cadena
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # powershell
 Ejercicios de powershell
+
+Warning: The following files have been created with the assistance of AI.
+1. estilo.css (link to 11.ps1) w/Gemini
