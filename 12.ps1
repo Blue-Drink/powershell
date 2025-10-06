708f86290b3d88774d896c858f7c7951652351fd
Blue-Drink <justme1random@gmail.com>
2025-10-06 10:43:26 +0200

    cual es mayor

--- 12.ps1
+++ 12.ps1
@@ -1,13 +1,14 @@
 # FUNCIONES
-function suma{
+function mayor{
     param(
         [int] $n1,
         [int] $n2
     )
-    $suma = $n1 + $n2
-    return $suma
+    if( $n1 -gt $n2 ){ $mayor = $n1 }
+    else{ $mayor = $n2 }
+    return $mayor
 }
 $n1 = read-host -prompt "Operando 1"
 $n2 = read-host -prompt "Operando 2"
-$resultado = suma -n1 $n1 -n2 $n2
+$resultado = mayor -n1 $n1 -n2 $n2
 write-host $resultado
