326da1298537ecd928384efbdb20710217045e99
Blue-Drink <justme1random@gmail.com>
2025-10-17 08:47:33 +0200

    cual id es el más alto de los procesos que se estan ejecutando

---
+++ 14.ps1
@@ -0,0 +1,13 @@
+# Cálculo del máximo
+
+$p = Get-Process
+$l = $p.length
+$max = 0
+
+for( $i = 0 ; $i -lt $l ; $i++ ){
+    $id = $p[ $i ].Id
+    if( $id -gt $max ){
+        $max = $id
+    }
+}
+Write-Host "El ID más alto es $max"
