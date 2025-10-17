4e9cbd710426aebe9e9b3136e06663036c93fe05
Blue-Drink <justme1random@gmail.com>
2025-10-17 09:08:05 +0200

    maximo tiempo de cpu de los procesos

---
+++ 16.ps1
@@ -0,0 +1,16 @@
+# Cálculo del máximo
+
+$p = Get-Process;
+$l = $p.length;
+$max = $p[ 0 ].CPU;
+$pr = $p[ 0 ].ProcessName;
+
+for( $i = 1 ; $i -lt $l ; $i++ ){
+    $cpu = $p[ $i ].CPU;
+    if( $cpu -gt $max ){
+        $max = $cpu;
+        $pr = $p[ $i ].ProcessName;
+    }
+};
+
+Write-Host "$pr tiene el tiempo máximo de CPU con $max segundos";
