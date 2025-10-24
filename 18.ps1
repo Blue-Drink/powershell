a5fe3b532e70fcbd8502aa90ddceb66cfb536b35
Blue-Drink <justme1random@gmail.com>
2025-10-24 09:03:29 +0200

    Muestra lineas no comentadas y no vacias

--- 18.ps1
+++ 18.ps1
@@ -2,5 +2,9 @@ $texto = Get-Content "C:\Windows\System32\drivers\etc\hosts";
 $l = $texto.Length;
 
 for( $i = 0 ; $i -lt $l ; $i++ ){
-    Write-Host $i "-" $texto[ $i ];
+    if( ! $texto[ $i ].StartsWith( "#" )  ){
+        $a = $texto[ $i ].trim();
+        if( $a -ne "" ){ Write-Host $i".-" $a };
+    }
+    
 };
