6a6cd657002bfd0082e481c9d11f91784cf897ab
Blue-Drink <justme1random@gmail.com>
2025-10-24 09:41:02 +0200

    va añadiendo en vez de guardar todo junto en una variable

--- 20.ps1
+++ 20.ps1
@@ -4,7 +4,7 @@ $l = $a.length;
 $cadena = "";
 for( $i = 0 ; $i -lt $l ; $i++ ){
     if( $a[ $i ].SI -eq 0 ){
-        $cadena += $a[ $i ].ProcessName + "`n";
+        $cadena = $a[ $i ].ProcessName;
+        Add-Content -Path $ruta -Value $cadena;
     }    
 }
-Set-Content -Path $ruta -Value $cadena;
