8e33b886911b2d3c637be20a64b9932692bcb7b5
Blue-Drink <justme1random@gmail.com>
2025-10-24 09:15:15 +0200

    menos variables

--- 19.ps1
+++ 19.ps1
@@ -2,6 +2,5 @@ $texto = Get-Content "D:\Repositorios\powershell\tareas.txt";
 $l = $texto.Length;
 for( $i = 0 ; $i -lt $l ; $i++ ){
     $p = Get-Process $texto[ $i ];
-    $id = $p.Id;
-    Stop-Process $id;
+    Stop-Process $p.Id;
 }
